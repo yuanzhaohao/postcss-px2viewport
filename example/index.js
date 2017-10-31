d773c9a015ebb6037b4b77fb65e803e8f5747c94
yuanzhaohao <yuanzhaohao@163.com>
2017-10-31 16:56:40 +0800

    feat: add example

---
+++ example/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var postcss = require('postcss');
+var px2viewport = require('..');
+var srcPath = path.join(__dirname, 'source.css');
+var destPath = path.join(__dirname, 'dest.css');
+var srcText = fs.readFileSync(srcPath, 'utf8');
+var outputText = postcss(px2viewport({ viewportWidth: 750 })).process(srcText).css;
+
+fs.writeFile(destPath, outputText, function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log('File with viewport units written.');
+});
