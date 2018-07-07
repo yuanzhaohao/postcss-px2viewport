a525c11fc659449476aa8864cbbdca917da44050
yuanzhaohao <yuanzhaohao@163.com>
2018-07-07 18:02:51 +0800

    feat: option isDeleteRem & option isDeleteDpr

--- example/index.js
+++ example/index.js
@@ -9,10 +9,12 @@ var destPath = path.join(__dirname, 'dest.css');
 var srcText = fs.readFileSync(srcPath, 'utf8');
 var outputText = postcss(px2viewport({
   viewportWidth: 375,
-  baseDpr: 1
+  baseDpr: 1,
+  isDeleteRem: true,
+  isDeleteDpr: true,
 })).process(srcText).css;
 
-fs.writeFile(destPath, outputText, function (err) {
+fs.writeFile(destPath, outputText, function(err) {
   if (err) {
     throw err;
   }
