9aba778360335ea38d5868e6f2fced845221909f
yuanzhaohao <yuanzhaohao@163.com>
2018-07-13 15:48:25 +0800

    fix: rem minimum value

--- example/index.js
+++ example/index.js
@@ -4,13 +4,13 @@ var fs = require('fs');
 var path = require('path');
 var postcss = require('postcss');
 var px2viewport = require('..');
-var srcPath = path.join(__dirname, 'source.css');
-var destPath = path.join(__dirname, 'dest.css');
+var srcPath = path.join(__dirname, 'source-sharing.css');
+var destPath = path.join(__dirname, 'dest-sharing.css');
 var srcText = fs.readFileSync(srcPath, 'utf8');
 var outputText = postcss(px2viewport({
   viewportWidth: 750,
   baseDpr: 2,
-  isDeleteRem: true,
+  // isDeleteRem: true,
   isDeleteDpr: true,
 })).process(srcText).css;
 
